21e080b1c850f543bc8d75bd2da950a463693e14
Dener Araujo (Natsume) <natsume2b.me@gmail.com>
2023-08-03 11:47:18 -0300

    creating transactions

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -1,17 +1,30 @@
-import crypto from 'node:crypto'
 import { knex } from '../database'
+import { z } from 'zod'
 import { FastifyInstance } from 'fastify'
+import { randomUUID } from 'node:crypto'
 
 export async function transactionsRoutes(app: FastifyInstance) {
-  app.get('/hello', async () => {
-    const transaction = await knex('transactions')
-      .insert({
-        id: crypto.randomUUID(),
-        title: 'test transaction',
-        amount: 1000,
-      })
-      .returning('*')
+  app.post('/', async (request, reply) => {
+    // {title, amount, type: credit or debit}
 
-    return transaction
+    const createTransactionBodySchema = z.object({
+      title: z.string(),
+      amount: z.number(),
+      type: z.enum(['credit', 'debit']),
+    })
+
+    const { title, amount, type } = createTransactionBodySchema.parse(
+      request.body,
+    )
+
+    await knex('transactions').insert({
+      id: randomUUID(),
+      title,
+      amount: type === 'credit' ? amount : amount * -1,
+    })
+
+    // 201 - resource successfully created
+
+    return reply.status(201).send()
   })
 }
